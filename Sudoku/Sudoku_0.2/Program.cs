6c4214d0d8d7ac35336e6b1a9ce7060481c92a99
LouisZhang <20212020089@fudan.edu.cn>
2020-09-29 19:09:27 +0800

    2020/09/29 周二

--- Sudoku/Sudoku_0.2/Program.cs
+++ Sudoku/Sudoku_0.2/Program.cs
@@ -3,12 +3,23 @@ using System.Collections;
 
 namespace Console_Try_1
 {
+
     class Sudoku9x9
     {
         private int[] content;// 0 for undefined, 1~9 for defined.
-        private Random rd = new Random(UnCheck((Int)DateTime.Now.Ticks));
+        private Random rd = new Random();
         public Sudoku9x9(){
             this.content = new int[81];
+        }
+
+        public Sudoku9x9 Clone(){
+            Sudoku9x9 result=new Sudoku9x9();
+            for(int r=0;r<9;r++){
+                for(int c=0;c<9;c++){
+                    result.set(r,c,this.get(r,c));
+                }
+            }
+            return result;
         }
 
         public int get(int r,int c){
@@ -61,29 +72,7 @@ namespace Console_Try_1
             int r=i/9;
             int c=i%9;
             //create the value list(make sure things in it are legal), and create a copy of itself.
-            ArrayList valueList = new ArrayList();
-            int ii,rr,cc;
-            bool flag=true;
-            for(ii=1;ii<=9;ii++){
-                flag=true;
-                for(rr=0;rr<9;rr++){
-                    // if (rr==r) continue;
-                    if (this.get(rr,c)==ii) {flag=false;break;}
-                }
-                for(cc=0;cc<9;cc++){
-                    // if (cc==c) continue;
-                    if (this.get(r,cc)==ii) {flag=false;break;}
-                }
-                for(int rp=0;rp<3;rp++){
-                    for(int cp=0;cp<3;cp++){
-                        // if ((rp==r%3)&&cp==c%3) continue;
-                        rr=rp+(r/3)*3;
-                        cc=cp+(c/3)*3;
-                        if (this.get(rr,cc)==ii) {flag=false;break;}
-                    }
-                }
-                if(flag) valueList.Add(ii);
-            }
+            ArrayList valueList = this.getLegalValues(r,c);
             if (valueList.Capacity==0) return 1;//solve failed.-------------
 
             // Sudoku9x9 selfCopy = new Sudoku9x9();
@@ -104,7 +93,7 @@ namespace Console_Try_1
             return 1;//solve failed.
         }
 
-        public int solveExh(int ceiling=2){//exhaustive solve
+        public int solveExh(int ceiling=2,bool show=true){//exhaustive solve
             int cnt=0;
             //find the first '0'
             int i=0;
@@ -112,7 +101,9 @@ namespace Console_Try_1
                 if(this.content[i]==0) break;
             }
             if (i==81) {
-                this.show();
+                if(show){
+                    this.show();
+                }
                 return 1;
                 //solve successful, 1 possibility found.-----------------------
             } 
@@ -121,6 +112,68 @@ namespace Console_Try_1
             int r=i/9;
             int c=i%9;
             //create the value list(make sure things in it are legal), and create a copy of itself.
+            ArrayList valueList = this.getLegalValues(r,c);
+            if (valueList.Capacity==0) return 0;//solve failed.-------------
+
+            // Sudoku9x9 selfCopy = new Sudoku9x9();
+            foreach(int v in valueList){
+                //fill the '0' with a value in the value list
+                this.set(r,c,v);
+                //a new sudoku was born! try to solve it.
+                //different from solve()
+                cnt+=this.solveExh();
+                if (cnt>=ceiling) return cnt;
+                this.set(r,c,0);
+            }
+
+            return cnt;//solve done.
+        }
+
+        // public int solveExh2(int ceiling=2){
+
+        //     return 0;
+        // }
+
+        public void generate(){
+            this.clear();
+            //randomSort
+            ArrayList listEmpty=new ArrayList();
+            ArrayList listOccup=new ArrayList();
+            for(int i=0;i<81;i++){
+                int r=i/9;
+                int c=i%9;
+                if(this.get(r,c)!=0) listEmpty.Add(i);
+                else listOccup.Add(i);
+            }
+            int index=listOccup.Capacity;
+            ArrayList randomList=randomSort(listOccup,this.rd);
+            foreach(int item in randomSort(listEmpty,this.rd)){
+                randomList.Add(item);
+            }
+            
+            bool flag=true;
+            while(flag){
+                int r=index/9;
+                int c=index%9;
+                foreach(int item in randomSort(this.getLegalValues(r,c),this.rd)){
+                    bool flag2=false;
+                    this.set(r,c,item);
+                    switch(this.solveExh(show: false)){
+                        case 0://conflicting restricts
+                        break;
+                        case 1://just enough restricts, or could be less. Prune.
+                        break;
+                        default://not enough restricts.
+                            index++;
+                            flag2=true;
+                        break;
+                    }
+                    if(flag2) break;
+                }
+            }
+        }
+
+        public ArrayList getLegalValues(int r,int c){
             ArrayList valueList = new ArrayList();
             int ii,rr,cc;
             bool flag=true;
@@ -144,61 +197,15 @@ namespace Console_Try_1
                 }
                 if(flag) valueList.Add(ii);
             }
-            if (valueList.Capacity==0) return 0;//solve failed.-------------
-
-            // Sudoku9x9 selfCopy = new Sudoku9x9();
-            foreach(int v in valueList){
-                //fill the '0' with a value in the value list
-                this.set(r,c,v);
-                //a new sudoku was born! try to solve it.
-                //different from solve()
-                cnt+=this.solveExh();
-                if (cnt>=ceiling) return cnt;
-                this.set(r,c,0);
-            }
-
-            return cnt;//solve done.
+            return valueList;
         }
-
-        public void generate(){
-            //randomSort
-            ArrayList tmpList=new ArrayList();
+        
+        public void clear(){
             for(int i=0;i<81;i++){
-                tmpList.Add(i);
-            }
-            ArrayList randomList=new ArrayList();
-            foreach (int item in tmpList) {
-                randomList.Insert(random.Next(randomList.Count), item);
-            }
-            foreach (int index in randomList){
-                ArrayList valueList0 = new ArrayList();
-                int ii,rr,cc;
-                bool flag=true;
-                for(ii=1;ii<=9;ii++){
-                    flag=true;
-                    for(rr=0;rr<9;rr++){
-                        // if (rr==r) continue;
-                        if (this.get(rr,c)==ii) {flag=false;break;}
-                    }
-                    for(cc=0;cc<9;cc++){
-                        // if (cc==c) continue;
-                        if (this.get(r,cc)==ii) {flag=false;break;}
-                    }
-                    for(int rp=0;rp<3;rp++){
-                        for(int cp=0;cp<3;cp++){
-                            // if ((rp==r%3)&&cp==c%3) continue;
-                            rr=rp+(r/3)*3;
-                            cc=cp+(c/3)*3;
-                            if (this.get(rr,cc)==ii) {flag=false;break;}
-                        }
-                    }
-                    if(flag) valueList0.Add(ii);
-                }
-                if (valueList0.Capacity==0) continue;
-                ArrayList valueList=new ArrayList();
-
+                this.content[i]=0;
             }
         }
+
 
         public void shuffle(){//just for test.
             for(int i=0;i<81;i++){
@@ -218,6 +225,15 @@ namespace Console_Try_1
                             2,0,0,0,0,0,4,0,0};
             this.content=(int [])tmp.Clone();
         }
+
+        ArrayList randomSort(ArrayList input, Random rd){
+            ArrayList result=new ArrayList();
+            foreach(var item in input){
+                result.Insert(rd.Next(0,result.Capacity),item);
+            }
+            return result;
+        }
+
     }
     class Program
     {
@@ -228,6 +244,7 @@ namespace Console_Try_1
             sudoku.sample();
             sudoku.show();
             if(sudoku.solveExh()>1) Console.WriteLine("Sudoku Error.");
+            sudoku.show();
         }
     }
 }
